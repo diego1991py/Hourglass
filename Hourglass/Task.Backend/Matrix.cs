315a8f4e249cf7a11010f347ab3931783a85e14d
Diego Valencia <91valenciadiego@gmail.com>
2026-03-08 11:34:00 -0500

    Write a program that fills a square matrix of order N (assume that N is always an odd number) with the following numbers (assuming that N equals 11).

---
+++ Hourglass/Task.Backend/Matrix.cs
@@ -0,0 +1,84 @@
+﻿namespace Task.Backend
+{
+    public class Matrix
+    {
+        private int _order;
+        private int[,] _matrix;
+
+        public Matrix(int order)
+        {
+            Order = order;
+            _matrix = new int[Order, Order];
+        }
+
+        public int Order
+        {
+            get => _order;
+            set => _order = ValidateOrder(value);
+        }
+
+        private int ValidateOrder(int order)
+        {
+            if (order <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(order),
+                    "The order of the matrix cannot be less than 1");
+            }
+
+            return order;
+        }
+
+        public void FillMatrix()
+        {
+            for (int i = 0; i < Order; i++)
+            {
+                for (int j = 0; j < Order; j++)
+                {
+                    _matrix[i, j] = i + j;
+                }
+            }
+        }
+
+        public void ShowMatrix()
+        {
+            for (int i = 0; i < Order; i++)
+            {
+                for (int j = 0; j < Order; j++)
+                {
+                    Console.Write($"{_matrix[i, j],4}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public void ShowHourGlass()
+        {
+            int mid = Order / 2;
+
+            for (int i = 0; i < Order; i++)
+            {
+                for (int j = 0; j < Order; j++)
+                {
+                    if (i <= mid)
+                    {
+                        if (j >= i && j <= Order - i - 1)
+                            Console.Write($"{_matrix[i, j],4}");
+                        else
+                            Console.Write("    ");
+                    }
+                    else
+                    {
+                        if (j >= Order - i - 1 && j <= i)
+                            Console.Write($"{_matrix[i, j],4}");
+                        else
+                            Console.Write("    ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ Hourglass/Task.Frontend/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Task.Backend;
+
+Console.Write("Ingrese orden de la matriz: ");
+int n = int.Parse(Console.ReadLine());
+
+Matrix matrix = new Matrix(n);
+
+matrix.FillMatrix();
+
+Console.WriteLine("MATRIZ COMPLETA");
+matrix.ShowMatrix();
+
+Console.WriteLine();
+Console.WriteLine("RELOJ DE ARENA");
+
+matrix.ShowHourGlass();
